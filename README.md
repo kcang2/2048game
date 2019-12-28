ccd41356b92e56848525db2a44dbbb5bf4287685
kcang2 <phillippang1994@gmail.com>
2019-12-28 20:15:35 +0800

    fixed multiple merge & tidied UI

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ based on the popular game by Gabriele Cirulli
 
 ## TO-DO(s)
 - [X] Prevent random generation of new tiles if a move made no change to the grid
-- [ ] Disable multiple merges eg: [2,2,2,2] -> [0,0,4,4] NOT [0,0,0,8]
+- [X] Disable multiple merges eg: [2,2,2,2] -> [0,0,4,4] NOT [0,0,0,8]
 
 ## Enhancement(s)
 - [ ] GUI
--- game.py
+++ game.py
@@ -34,12 +34,16 @@ def apply_move(move, score, grid):
             for i in range(1, grid.shape[0]):  # for each row except 1st
                 p = i
                 while 0 < p < grid.shape[0]:  # 1 to 3
-                    if (grid[p-1, j] == 0) or (grid[p-1, j] == grid[p, j]):  # next space is 0 or same
-                        score += 2*grid[p-1, j]  # update score
+                    if grid[p-1, j] == 0:  # next space is 0
                         grid[p-1, j] = grid[p-1, j] + grid[p, j]  # move it
                         grid[p, j] = 0  # make the previous empty
                         p -= 1  # keep moving
-                    else:  # Different value but not empty also
+                    elif grid[p-1, j] == grid[p, j]:  # next space is same
+                        score += 2 * grid[p - 1, j]  # update score
+                        grid[p-1, j] = grid[p-1, j] + grid[p, j]  # move it
+                        grid[p, j] = 0  # make the previous empty
+                        break
+                    else:  # Different value
                         break  # can no longer move
 
     elif move == 's':
@@ -47,12 +51,16 @@ def apply_move(move, score, grid):
             for i in range(grid.shape[0]-2, -1, -1):  # for each row except last
                 p = i  # pointer
                 while 0 <= p < grid.shape[0]-1:  # 0 to 2
-                    if (grid[p+1, j] == 0) or (grid[p+1, j] == grid[p, j]):  # next space is 0 or same
-                        score += 2*grid[p+1, j]  # update score
+                    if grid[p+1, j] == 0:  # next space is 0
                         grid[p+1, j] = grid[p+1, j] + grid[p, j]  # move it
                         grid[p, j] = 0  # make the previous empty
                         p += 1  # keep moving
-                    else:  # Different value but not empty also
+                    elif grid[p + 1, j] == grid[p, j]:  # next space is same
+                        score += 2 * grid[p + 1, j]  # update score
+                        grid[p+1, j] = grid[p+1, j] + grid[p, j]  # move it
+                        grid[p, j] = 0  # make the previous empty
+                        break
+                    else:  # Different value
                         break  # can no longer move
 
     elif move == 'a':
@@ -60,12 +68,16 @@ def apply_move(move, score, grid):
             for j in range(1, grid.shape[1]):  # for each column except 1st
                 p = j
                 while 0 < p < grid.shape[1]:  # 1 to 3
-                    if (grid[i, p-1] == 0) or (grid[i, p-1] == grid[i, p]):  # next space is 0 or same
-                        score += 2*grid[i, p-1]  # update score
+                    if grid[i, p-1] == 0:  # next space is 0
                         grid[i, p-1] = grid[i, p-1] + grid[i, p]  # move it
                         grid[i, p] = 0  # make the previous empty
                         p -= 1  # keep moving
-                    else:  # Different value but not empty also
+                    elif grid[i, p-1] == grid[i, p]:  # next space is same
+                        score += 2 * grid[i, p - 1]  # update score
+                        grid[i, p-1] = grid[i, p-1] + grid[i, p]  # move it
+                        grid[i, p] = 0  # make the previous empty
+                        break
+                    else:  # Different value
                         break  # can no longer move
 
     elif move == 'd':
@@ -73,12 +85,16 @@ def apply_move(move, score, grid):
             for j in range(grid.shape[1]-2, -1, -1):  # for each column except last
                 p = j
                 while 0 <= p < grid.shape[1]-1:  # 0 to 2
-                    if (grid[i, p+1] == 0) or (grid[i, p+1] == grid[i, p]):  # next space is 0 or same
-                        score += 2*grid[i, p+1]  # update score
+                    if grid[i, p+1] == 0:  # next space is 0 or same
                         grid[i, p+1] = grid[i, p+1] + grid[i, p]  # move it
                         grid[i, p] = 0  # make the previous empty
                         p += 1  # keep moving
-                    else:  # Different value but not empty also
+                    elif grid[i, p+1] == grid[i, p]:
+                        score += 2 * grid[i, p + 1]  # update score
+                        grid[i, p+1] = grid[i, p+1] + grid[i, p]  # move it
+                        grid[i, p] = 0  # make the previous empty
+                        break
+                    else:  # Different value
                         break  # can no longer move
 
     return False if np.array_equal(temp, grid) else random_loc(grid), score
@@ -98,14 +114,15 @@ if __name__ == "__main__":
         grid[y, x] = 2
 
     while not gameover:
+        print("Score: " + str(score))
         print(grid)
-        print("Score: "+str(score))
         move = input("Move: w=up, s=down, a=left, d=right")
         if move not in valids:
             print("Invalid Move")
             continue
         else:
             gameover, score = apply_move(move, score, grid)
+        print("="*60)
 
     print("GAME OVER!")
     print("Score: "+str(score))
